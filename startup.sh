f4bdf377232fabd122dd223eaab47c81d6d1e0d0
omamaMhd <omama.mohamad2002@gmail.com>
2026-05-31 23:39:02 +0300

    azure

--- startup.sh
+++ startup.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 echo "Starting app..."
-
 cd /home/site/wwwroot
 
+# توليد الـ الكاش لزيادة السرعة وتجنب المشاكل في بيئة الإنتاج
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
+# ربط التخزين
 php artisan storage:link
 
-php-fpm
+# تشغيل السيرفر الافتراضي لـ Azure (خلف الكواليس)
